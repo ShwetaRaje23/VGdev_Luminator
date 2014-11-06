e99ae8eef8e91b0fa72657938553eec02229f552
ShwetaRaje <shweta.raje@gatech.edu>
2014-11-05 20:07:18 -0500

    Added sinkTrigger and PlayerControl script [TBD]
    sinkTrigger - a sphere with collider
    playerControl detects the collision and reduces its y co-ordinates --
    [not solved]

---
+++ LuminatorGame/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControl : MonoBehaviour {
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	void OnControllerColliderHit(ControllerColliderHit hit) {
+		if (hit.gameObject.tag == "sinkTrigger") {
+			
+			Debug.Log("Collision");
+			sinkEffect.entertriggersink = true;
+			
+		}
+		
+	}
+}
---
+++ LuminatorGame/Assets/Scripts/sinkEffect.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class sinkEffect : MonoBehaviour {
+
+	// Use this for initialization
+	public static bool entertriggersink = false;
+	float height;
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+			
+		if (entertriggersink == true) {
+
+			GameObject player = GameObject.Find("Player");
+
+			Debug.Log(" Sinking !! ");
+
+			while (height > 0f)
+			{
+				height -= player.transform.position.y * Time.deltaTime; 
+				player.transform.position = new Vector3(transform.position.x, height, transform.position.z);
+			}
+		//		transform.position = Vector3.Lerp(transform.position, 	);
+		}
+	}
+
+
+}
