560752e93dd11873bd230201500b4ac2cba17ccd
Smahendru <smahendru3@gatech.edu>
2014-11-08 18:15:46 -0500

    Environment updated. Refined maze path. Added pickups, one food and one stick. Many weapons and food items present in imported pacakge. Sonal

---
+++ LuminatorGame/Assets/Scripts/Rotator.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotator : MonoBehaviour {
+
+	private Color[] colors = {Color.red,Color.blue,Color.gray,Color.green,Color.magenta,Color.black,Color.yellow,Color.cyan,Color.white};
+
+	void Start()
+	{
+				transform.renderer.material.color = colors [Random.Range (0, 8)];
+		}
+	// Update is called once per frame
+	void Update () {
+	
+		transform.Rotate (new Vector3 (15, 30, 45) * Time.deltaTime);
+		int randomNbr = Random.Range (0, 100);
+		if(randomNbr > 70 && randomNbr < 90) //included this logic to slightly slower the speed with which colors change
+			transform.renderer.material.color = colors [Random.Range (0, 8)];		
+	}
+}
